c2282e36619f1bf19c3dda340431fcd8cc642182
jaysenjonsin <jasoncjohnson5@yahoo.com>
2023-09-02 23:36:13 -0700

    working carousel

---
+++ src/components/buttons/NextButton.js
@@ -0,0 +1,14 @@
+import styles from './NextButton.module.css';
+
+const NextButton = ({ onClick }) => {
+  return (
+    <div className={styles.next}>
+      {/* Apply the onClick prop to the button */}
+      <button onClick={onClick} className={styles.next1}>
+        HELLOOOOOO NEXT BUTTON
+      </button>
+    </div>
+  );
+};
+
+export default NextButton;
--- src/pages/game.js
+++ src/pages/game.js
@@ -1,13 +1,42 @@
-import React from 'react';
-import styles from './game.css';
+import React, { useState } from 'react';
+import styles from './game.css'; // Ensure the correct path to your CSS file
+import Orv9 from '../components/planes/Orv9';
+import Neha1 from '../components/planes/Neha1';
+import Melia from '../components/planes/Melia';
+import Tom6 from '../components/planes/Tom6';
+import Will7 from '../components/planes/Will7';
+import Mav5 from '../components/planes/Mav5';
+import Buster from '../components/planes/Buster';
+import NextButton from '../components/buttons/NextButton';
+import PreviousButton from '../components/buttons/PreviousButton';
 
 export default function Game() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const planes = [Orv9, Neha1, Melia, Tom6, Will7, Mav5, Buster];
+
+  const CurrentPlane = planes[currentIndex];
+
+  const handlePrev = () => {
+    setCurrentIndex((prev) => (prev === 0 ? 0 : prev - 1));
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prev) =>
+      prev === planes.length - 1 ? planes.length - 1 : prev + 1
+    );
+  };
+
   return (
     <div className={styles.backgroundContainer}>
       <video autoPlay loop muted playsInline>
-        <source src="/background.mp4" type="video/mp4" />
+        <source src='/background.mp4' type='video/mp4' />
         Your browser does not support the video tag.
       </video>
+      <CurrentPlane />
+      <div>
+        <PreviousButton onClick={handlePrev} />
+        <NextButton onClick={handleNext} />
+      </div>
     </div>
   );
 }
