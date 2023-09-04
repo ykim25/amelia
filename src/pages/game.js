e04041f55cc4b1ba02beea2c4030c74c90a56719
Yuri Kim <ymail.kim@gmail.com>
2023-09-03 19:20:04 -0700

    removed unnecessary imports

--- src/pages/game.js
+++ src/pages/game.js
@@ -1,25 +1,6 @@
 import React, { useState } from 'react';
 import styles from './game.css';
-
-// Planes
-import Orv9 from '../pages/components/planes/Orv9.png';
-import Neha1 from '../pages/components/planes/Neha1.png';
-import Melia from '../pages/components/planes/Melia.png';
-import Tom6 from '../pages/components/planes/Tom6.png';
-import Will7 from '../pages/components/planes/Will7.png';
-import Mav5 from '../pages/components/planes/Mav5.png';
-import Buster from '../pages/components/planes/Buster.png';
-
-// Buttons
-import Launch from '../pages/components/buttons/Launch.png';
-import PlayAgain from '../pages/components/buttons/PlayAgain.png';
 import PlayButton from '../pages/components/buttons/PlayButton.png';
-
-// Popups
-import Win from '../pages/components/popups/Win.png';
-import Lose from '../pages/components/popups/Lose.png';
-
-// Logo
 import Logo from '../pages/components/logo/AmeliaLogo.png'
 
 export default function Game() {
