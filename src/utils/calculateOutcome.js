0b875e11285f6a0a87305f83dbbf8b2f3f553d32
jaysenjonsin <jasoncjohnson5@yahoo.com>
2023-09-03 17:03:16 -0700

    fix comments

--- src/utils/calculateOutcome.js
+++ src/utils/calculateOutcome.js
@@ -6,32 +6,6 @@ import Orv9 from '../components/planes/Orv9';
 import Tom6 from '../components/planes/Tom6';
 import Will7 from '../components/planes/Will7';
 
-// export const calculateOutcome = (currentPlane) => {
-//   let baseScore = 0;
-//   let windScore = 0; // = nehas fetch result (either 2, 0, or -2)
-//   let animationStyle;
-//   console.log(`current plane: ${currentPlane}`);
-//   if (currentPlane == 'Orv9') baseScore = -2;
-//   else if (currentPlane === 'Neha1') baseScore = 4;
-//   else if (currentPlane === 'Melia') baseScore = 2;
-//   else if (currentPlane === 'Tom6') baseScore = 1;
-//   else if (currentPlane === 'Will7') baseScore = 3;
-//   else if (currentPlane === 'Mav5') baseScore = 2;
-//   else if (currentPlane === 'Buster') baseScore = 4;
-
-//   console.log(`base score: ${baseScore}`);
-
-//   let totalScore = baseScore + windScore;
-//   //if statements: if score > 0 and currentPlane = neha1, return video x -- etc.
-//   if (totalScore < 0) animationStyle = 'crash';
-//   if (totalScore === 1 && totalScore <= 3) animationStyle = 'water';
-//   if (totalScore === 4) animationStyle = 'win';
-//   if (totalScore > 4) animationStyle = 'offScreen';
-
-//   return `${currentPlane} ${animationStyle}`;
-
-// };
-
 export const calculateOutcome = (currentPlane) => {
   const baseScores = {
     Orv9: -2,
